228584f74d8955cc555307c18fd889d11fdac985
Sujonmia5 <mdsujon673738@gmail.com>
2023-06-25 00:15:26 +0600

    landing-page: home section completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,17 @@
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import LandingPage from "./pages/LandingPage";
-import Body1 from "./components/Body/body1";
-import Body2 from "./components/Body/body2";
-import Body3 from "./components/Body/body3";
+// import Body1 from "./components/Body/body1";
+// import Body2 from "./components/Body/body2";
+// import Body3 from "./components/Body/body3";
 
 function App() {
   return (
     <>
       <Navbar />
-      <Body1 />
+      {/* <Body1 />
       <Body2 />
-      <Body3 />
+      <Body3 /> */}
       <Routes>
         <Route path="/" element={<LandingPage />} />
       </Routes>
